fb909510f8eaca71f7105d9e3b023f617966ff3c
erennc <l3320042@std.yildiz.edu.tr>
2025-10-01 18:44:10 +0300

    Fix: Eleventy config yol sorunlarını düzelt

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 ﻿module.exports = function(eleventyConfig) {
+  // Markdown dosyalarının içindeki resimlerin çalışması için ek ayar
+  eleventyConfig.addPassthroughCopy("testler/**/*.png");
+  eleventyConfig.addPassthroughCopy("testler/**/*.jpg");
+
   return {
     dir: {
-      input: "testler",
-      includes: "_includes",
+      input: ".", // Ana dizini girdi olarak kabul et
+      includes: "_includes", // Şablonlar bu klasörde
       output: "_site"
-    }
+    },
+    // Tüm markdown dosyalarını HTML'e çevir
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
 };
-
